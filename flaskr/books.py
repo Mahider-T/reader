e77a9c18e4b8494e378e45adb94acac504cb39d8
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-07-31 05:58:38 +0300

    Initializing the books db

--- flaskr/books.py
+++ flaskr/books.py
@@ -9,18 +9,20 @@ from flaskr.db import get_db
 
 bp = Blueprint('books', __name__, url_prefix = '/books')
 
-db = get_db()
-
 def get_books_table_header():
     with open("books.csv", "r") as csvfile:
 
-        reader = csv.reader(csvfile, skiprows = 1)
+        reader = csv.reader(csvfile)
         return reader
         
 # numberOfRows = len(get_books_table_header())
+def add_books():
+    db = get_db()
+    for row in reader:
+        db.execute("INSERT INTO books VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",(row[0], row[1],row[2], row[3],row[4], row[5],row[6], row[7],row[8], row[9],row[10], row[11]))
+        db.commit()
 
-for row in reader:
-    db.execute("INSERT INTO books VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",(row[0], row[1],row[2], row[3],row[4], row[5],row[6], row[7],row[8], row[9],row[10], row[11]))
-    db.commit()
-    
-print(numberOfRows)
+@bp.route("/")
+def hi():
+    return hi
+# print(numberOfRows)

bd067bf16a477f50ece13965d825baba61385ee5
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-07-31 06:42:05 +0300

    Initialize books table of the db

--- flaskr/books.py
+++ flaskr/books.py
@@ -8,21 +8,21 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 bp = Blueprint('books', __name__, url_prefix = '/books')
-
-def get_books_table_header():
-    with open("books.csv", "r") as csvfile:
-
-        reader = csv.reader(csvfile)
-        return reader
         
-# numberOfRows = len(get_books_table_header())
-def add_books():
-    db = get_db()
-    for row in reader:
-        db.execute("INSERT INTO books VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",(row[0], row[1],row[2], row[3],row[4], row[5],row[6], row[7],row[8], row[9],row[10], row[11]))
-        db.commit()
+#The below route was used to initialize the books table for the first time by reading from a csv file
+# @bp.route("/add")
+# def add_books():
+#     db = get_db()
+#     with open("/home/oogway/reader/flaskr/books.csv", "r") as csvfile:
+#         reader = csv.reader(csvfile)
+#         next(reader, None)
+#         for row in reader:
+#             db.execute("INSERT INTO books VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",(row[0], row[1], row[2], row[3], row[4], row[5],row[6], row[7], row[8], row[9], row[10], row[11]))
+#             db.commit()
+#     return "Succuss!"
 
-@bp.route("/")
+
+@bp.route("/hi")
 def hi():
-    return hi
+    return "hi"
 # print(numberOfRows)

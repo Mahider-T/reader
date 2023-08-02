a292562b9945e822b5ca0610183dc3c16c018ccb
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-08-02 15:18:00 +0300

    Change the create connection to database function

--- flaskr/db.py
+++ flaskr/db.py
@@ -3,15 +3,21 @@ import sqlite3
 import click
 from flask import current_app, g
 
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types = sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
+# def get_db():
+    # if 'db' not in g:
+    #     g.db = sqlite3.connect(
+    #         current_app.config['DATABASE'],
+    #         detect_types = sqlite3.PARSE_DECLTYPES
+    #     )
+    #     g.db.row_factory = sqlite3.Row
     
-    return g.db
+DATABASE = '/home/oogway/reader/instance/flaskr.sqlite'
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
 
 def close_db(e = None):
     db = g.pop('db', None)
@@ -20,7 +26,6 @@ def close_db(e = None):
 
 def init_db():
     db = get_db()
-
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 

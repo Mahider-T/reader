bbcfa2c4ff64f3d44bab7ed6db0b099fa74c15b4
Mahider-T <95926290+Mahider-T@users.noreply.github.com>
2023-08-02 15:21:14 +0300

    Add book searching endpoint

--- flaskr/books.py
+++ flaskr/books.py
@@ -1,7 +1,8 @@
 import csv
 import functools
+import sqlite3
 
-from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
+from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for, current_app,jsonify)
 
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -17,12 +18,14 @@ bp = Blueprint('books', __name__, url_prefix = '/books')
 #         reader = csv.reader(csvfile)
 #         next(reader, None)
 #         for row in reader:
-#             db.execute("INSERT INTO books VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",(row[0], row[1], row[2], row[3], row[4], row[5],row[6], row[7], row[8], row[9], row[10], row[11]))
+            # db.execute("INSERT INTO books VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",(row[0], row[1], row[2], row[3], row[4], row[5],row[6], row[7], row[8], row[9], row[10], row[11]))
 #             db.commit()
 #     return "Succuss!"
 
-
-@bp.route("/hi")
-def hi():
-    return "hi"
-# print(numberOfRows)
+@bp.route("/find")
+def find():
+    db = get_db()
+    find = request.form["find"]
+    cursor = db.execute("SELECT title FROM books WHERE title LIKE ?", (f"{find}%",))
+    titles = cursor.fetchall()
+    return titles
